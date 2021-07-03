aad2312845a154b2a6d47f7e55b35d79c844f4d6
katsuki okuno <kokuno173@gmail.com>
2021-07-03 12:09:09 +0900

    modified plot function

--- plot.py
+++ plot.py
@@ -1,7 +1,28 @@
+import numpy as np
 from matplotlib import pyplot as plt
 import seaborn as sns
 
-def plot_sample(sample, bins=20, xlim=(0., 1.), ylim=(0., 10.), element='step', binrange=((0., 1.), (0., 1.), (0., 1.))):
+def plot_sample(sample, bins=20, xlim=(0., 1.), element='step'):
+    bin_min = np.round(np.sort(sample, axis=0)[int(0.001*sample.shape[0])], decimals=2)
+    bin_min = np.floor(bin_min*10)/10
+    bin_max = np.round(np.sort(sample, axis=0)[int(0.999*sample.shape[0])], decimals=2)
+    bin_max = np.ceil(bin_max*10)/10
+
+    hist_max = []
+    for i in range(sample.shape[1]):
+        hist = np.histogram(sample[:, i], bins=bins, range=(bin_min[i], bin_max[i]), density=True)
+        hist_max.append(np.max(hist[0]))
+    hist_max = max(hist_max)
+    if hist_max <= 5:
+        hist_max = np.ceil(hist_max)
+    elif hist_max <= 10:
+        hist_max = np.ceil(hist_max/2)*2
+    elif hist_max <= 20:
+        hist_max = np.ceil(hist_max/5)*5
+    else:
+        hist_max = np.ceil(hist_max/10)*10
+    
+
     plt.figure(figsize=(10, 6))
     plt.suptitle('Prior Distribution')
 
@@ -12,10 +33,10 @@ def plot_sample(sample, bins=20, xlim=(0., 1.), ylim=(0., 10.), element='step', 
     plt.legend(loc='upper right')
 
     plt.subplot(322)
-    sns.histplot(sample[:, 0], stat='density', bins=bins, binrange=binrange[0],
+    sns.histplot(sample[:, 0], stat='density', bins=bins, binrange=(bin_min[0], bin_max[0]),
                 label='R', color='blue', element=element)
     plt.xlabel('p'), plt.ylabel('pdf')
-    plt.xlim(xlim), plt.ylim(ylim)
+    plt.xlim(xlim), plt.ylim((0, hist_max))
     plt.legend()
 
     plt.subplot(323)
@@ -25,10 +46,10 @@ def plot_sample(sample, bins=20, xlim=(0., 1.), ylim=(0., 10.), element='step', 
     plt.legend(loc='upper right')
 
     plt.subplot(324)
-    sns.histplot(sample[:, 1], stat='density', bins=bins, binrange=binrange[1],
+    sns.histplot(sample[:, 1], stat='density', bins=bins, binrange=(bin_min[1], bin_max[1]),
                 label='SR', color='red', element=element)
     plt.xlabel('p'), plt.ylabel('pdf')
-    plt.xlim(xlim), plt.ylim(ylim)
+    plt.xlim(xlim), plt.ylim((0, hist_max))
     plt.legend()
 
     plt.subplot(325)
@@ -38,10 +59,10 @@ def plot_sample(sample, bins=20, xlim=(0., 1.), ylim=(0., 10.), element='step', 
     plt.legend(loc='upper right')
 
     plt.subplot(326)
-    sns.histplot(sample[:, 2], stat='density', bins=bins, binrange=binrange[2],
+    sns.histplot(sample[:, 2], stat='density', bins=bins, binrange=(bin_min[2], bin_max[2]),
                 label='SSR', color='green', element=element)
     plt.xlabel('p'), plt.ylabel('pdf')
-    plt.xlim(xlim), plt.ylim(ylim)
+    plt.xlim(xlim), plt.ylim((0, hist_max))
     plt.legend()
 
     plt.tight_layout()
